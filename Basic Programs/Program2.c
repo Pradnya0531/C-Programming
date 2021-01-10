21b71fed1890fc550fe85f9ac91647b6880778c8
Pradnya0531 <65937088+Pradnya0531@users.noreply.github.com>
2021-01-10 15:06:20 +0530

    Create Program2.c

---
+++ Basic Programs/Program2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+     char cLetter='A';
+      for(cLetter='A';cLetter<='Z';cLetter++)
+        {
+          printf("%c\n",cLetter);
+        }
+printf("*****End Of Program*****\n");
+return 0;
+}
