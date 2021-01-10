e7d268ee2ba47954375845767564418655b9ea9a
Pradnya0531 <65937088+Pradnya0531@users.noreply.github.com>
2021-01-10 15:19:02 +0530

    Create Program4.c

---
+++ Basic Programs/Program4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   char cChar1=0;
+   for(cChar1=65;cChar1<=90;cChar1++)
+       {
+         printf("%d = %c \n",cChar1,cChar1);
+       }
+printf("*************End Of Program************\n");
+return 0;
+}
