4173a804752ccc9f2bad325d3d24fd346872a453
Pradnya0531 <65937088+Pradnya0531@users.noreply.github.com>
2021-01-11 01:55:47 +0530

    Create Program13.c

---
+++ Basic Programs/Program13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+Display()
+{
+   int i =5;
+   while(i>=0)
+    { 
+      printf("%d",i);
+      i--;
+    }
+}
+int main()
+{
+   Display();
+printf("******End of Program******\n");
+return 0;
+}
