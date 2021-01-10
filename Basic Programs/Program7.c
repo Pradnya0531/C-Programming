da61362d7b69763c581fc73013fd02686398d5ab
Pradnya0531 <65937088+Pradnya0531@users.noreply.github.com>
2021-01-10 15:28:11 +0530

    Create Program7.c

---
+++ Basic Programs/Program7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int iVal=0;
+   for(iVal=65;iVal<=90;iVal++)
+     {
+       printf("%c's Ascii Value Is %u and Address is %d\n",iVal,iVal,&iVal);
+     }
+printf("*********End Of Program**********\n");
+return 0;
+}
