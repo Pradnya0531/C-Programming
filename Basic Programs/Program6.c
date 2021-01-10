43af251bc38453e282d26ff04f28df2cf52c56d1
Pradnya0531 <65937088+Pradnya0531@users.noreply.github.com>
2021-01-10 15:25:52 +0530

    Create Program6.c

---
+++ Basic Programs/Program6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int iCharacter =0;
+    for(iCharacter=0;iCharacter<=127;iCharacter++)
+      {
+       printf("%c's Ascii Value Is %d\n",iCharacter,iCharacter);
+      }
+Printf("**************End of Program**************\n");
+return 0;
+}
