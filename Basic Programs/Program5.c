c387dc7bb041cee3dec46e6b4a8e0b7c8ead099a
Pradnya0531 <65937088+Pradnya0531@users.noreply.github.com>
2021-01-10 15:22:10 +0530

    Create Program5.c

---
+++ Basic Programs/Program5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int iValue=0;
+   for(iValue=65;iValue<=90;iValue++)
+     {
+       printf("%c's Ascci Value Is %d\n",iValue,iValue);
+     }
+printf("******************End Of Program****************\n");
+return 0;
+}
