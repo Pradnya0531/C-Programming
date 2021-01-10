3207f2189eea62b6e1de6dcaba0041345de1f365
Pradnya0531 <65937088+Pradnya0531@users.noreply.github.com>
2021-01-10 15:00:38 +0530

    Create Program1.c

---
+++ Basic Programs/Program1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int iAscii=0;
+     for(iAscii=65;iAscii<=90;iAscii++)
+      {
+        printf("%c\n",iAscii);
+      }
+printf("********End of Program********\n");
+return 0;
+}
